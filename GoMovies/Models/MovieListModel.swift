6f6193acd157a68474d98235f3d05012da8581c3
Anshul Vyas <anshul.vyas380@gmail.com>
2020-06-16 13:38:53 +0530

    Add Failable Decodable to filter corrupt data elements

--- GoMovies/Models/MovieListModel.swift
+++ GoMovies/Models/MovieListModel.swift
@@ -14,6 +14,21 @@ struct MovieListModel: Decodable {
     let totalPages: Int
     let results: [Movie]
 
+    enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults
+        case totalPages
+        case results
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.page = try container.decode(Int.self, forKey: .page)
+        self.totalResults = try container.decode(Int.self, forKey: .totalResults)
+        self.totalPages = try container.decode(Int.self, forKey: .totalPages)
+        self.results = try container.decode([FailableDecodable<Movie>].self, forKey: .results).compactMap { $0.base }
+    }
+
     struct Movie: Decodable {
         let id: Int
         let title: String
